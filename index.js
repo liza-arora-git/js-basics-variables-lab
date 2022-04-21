ff40eda2b719a70737597a913b7d616ddc36c213
Liza Arora <liza.arora@outlook.com>
2022-04-22 09:45:54 +1000

    Variables Lab Commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+
+let mostProfitableNeighborhood;
+mostProfitableNeighborhood = 'Chelsea';
+
+// function mostProfitableNeighborhood(){
+//     let mostProfitableNeighborhood;
+//     mostProfitableNeighborhood() == 'Chelsea';
+// }
+
+let companyCeo;
+companyCeo = 'Susan Smith';
